db0b0a3bf8710bef26fdcf13d4b482f7f8facf86
Eric Dang <edang830@gmail.com>
2021-02-13 13:52:10 -0800

    MicroController only has one terminal pinset

--- cpp/Autogarden/include/components/microcontroller.hpp
+++ cpp/Autogarden/include/components/microcontroller.hpp
@@ -6,91 +6,47 @@
 class MicroController : public Component
 {
 public:
-    MicroController(const std::string& id, ITerminalPinSet<DigitalPin>* digitalPins,
-                    ITerminalPinSet<AnalogOutputPin>* analogOutputPins,
-                    ITerminalPinSet<AnalogInputPin>* analogInputPins) :
-        Component(id),
-        __mDigitalPins(digitalPins),
-        __mAnalogOutputPins(analogOutputPins),
-        __mAnalogInputPins(analogInputPins)
-    {
-    }
+    MicroController(const std::string& id, ITerminalPinSet* pins) : Component(id), __pPins(pins) {}
 
     virtual ~MicroController() = default;
 
-    void run() override { __mDigitalPins->refresh(); }
-
-    bool hasNumAvailableOutputPins(const int& requestedNum, const PinMode& pinMode) override
-    {
-        if (pinMode == PinMode::Digital)
-        {
-            return requestedNum <= __mDigitalPins->getNumAvailable();
-        }
-        else if (pinMode == PinMode::AnalogOutput)
-        {
-            return requestedNum <= __mAnalogOutputPins->getNumAvailable();
-        }
-        else if (pinMode == PinMode::AnalogInput)
-        {
-            return requestedNum <= __mAnalogInputPins->getNumAvailable();
-        }
-
-        return false;
-    }
-
-    std::vector<PinView> getNumAvailableOutputPins(const int& requestedNum, const PinMode& pinMode) override
-    {
-        if (pinMode == PinMode::Digital)
-        {
-            return __mDigitalPins->getNextAvailable(requestedNum);
-        }
-        else if (pinMode == PinMode::AnalogOutput)
-        {
-            return __mAnalogOutputPins->getNextAvailable(requestedNum);
-        }
-        else if (pinMode == PinMode::AnalogInput)
-        {
-            return __mAnalogInputPins->getNextAvailable(requestedNum);
-        }
-
-        return std::vector<PinView>();
-    }
-
-    std::vector<uint8_t> getDigitalPinNums() const { return __mDigitalPins->getPinNumbers(); }
-
-    std::vector<uint8_t> getAnalogOutputPinNums() const { return __mAnalogOutputPins->getPinNumbers(); }
-
-    std::vector<uint8_t> getAnalogInputPinNums() const { return __mAnalogInputPins->getPinNumbers(); }
-
-    int getAvailableDigitalPins() const { return __mDigitalPins->getNumAvailable(); }
-
-    int getAvailableAnalogOutputPins() const { return __mAnalogOutputPins->getNumAvailable(); }
-
-    int getAvailableAnalogInputPins() const { return __mAnalogInputPins->getNumAvailable(); }
+    void run() override { __pPins->refresh(); }
 
 protected:
-    bool _setInputPins(Component* component) override
+    bool _setInputPins(IOutputPinSet* parentOutputPins) override
     {
         return false;  // MicroController doesn't have any inputs. This makes it so it is always the root of the tree.
     }
 
+    IOutputPinSet* _getOutputPins() override { return __pPins; }
+
 private:
-    ITerminalPinSet<DigitalPin>* __mDigitalPins;
-    ITerminalPinSet<AnalogOutputPin>* __mAnalogOutputPins;
-    ITerminalPinSet<AnalogInputPin>* __mAnalogInputPins;
+    ITerminalPinSet* __pPins;
 };
 
 class MicroControllerFactory
 {
 public:
+    template <typename T>
+    void createPins(std::vector<ITerminalPin*>& pins, const std::initializer_list<uint8_t>& pinNums)
+    {
+        for (const auto& pinNum : pinNums)
+        {
+            pins.push_back(new T(pinNum));
+        }
+    }
+
     std::unique_ptr<MicroController> createMicroController(const std::string& id,
                                                            const std::initializer_list<uint8_t>& digital,
                                                            const std::initializer_list<uint8_t>& analogOutput,
                                                            const std::initializer_list<uint8_t>& analogInput)
     {
-        auto digitalPins      = createTerminalPinSet<DigitalPin>(digital);
-        auto analogOutputPins = createTerminalPinSet<AnalogOutputPin>(analogOutput);
-        auto analogInputPins  = createTerminalPinSet<AnalogInputPin>(analogInput);
-        return std::make_unique<MicroController>(id, digitalPins, analogOutputPins, analogInputPins);
+        std::vector<ITerminalPin*> pins;
+        pins.reserve(digital.size() + analogOutput.size() + analogInput.size());
+        createPins<DigitalPin>(pins, digital);
+        createPins<AnalogOutputPin>(pins, digital);
+        createPins<AnalogInputPin>(pins, digital);
+        auto pinSet = new TerminalPinSet(std::move(pins));
+        return std::make_unique<MicroController>(id, pinSet);
     }
 };
