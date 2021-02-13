cfafdf27727ea38ff599e3b5edf3d00dfdc2be80
Eric Dang <edang830@gmail.com>
2021-02-12 23:09:18 -0800

    Implement DigitalPin

---
+++ cpp/Autogarden/include/pins/digital_pin.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <Arduino.h>
+
+#include <pins/terminal_pin.hpp>
+
+class DigitalPin : public TerminalPin
+{
+public:
+    DigitalPin(const uint8_t& pin, const int& value = LOW) : TerminalPin(pin, value) {}
+
+    ~DigitalPin() = default;
+
+    PinMode getMode() const override { return Digital; }
+
+    void setValue(const int& value) override
+    {
+        Pin::setValue(value);
+        _mIsStale = true;
+    }
+
+    void refresh() override
+    {
+        digitalWrite(getPin(), getValue());
+        _mIsStale = false;
+    }
+
+protected:
+    int _scaleValue(const int& value) override { return value > LOW ? HIGH : LOW; }
+};
