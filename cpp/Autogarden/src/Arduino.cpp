6eb3b7d4d0daba8c12a28bac3a8664e0f760580d
Eric Dang <edang830@gmail.com>
2021-02-12 23:11:15 -0800

    Add mock arduino library

---
+++ cpp/Autogarden/src/Arduino.cpp
@@ -0,0 +1,55 @@
+#include <Arduino.h>
+
+#include <iostream>
+
+void setMockArduino(ArduinoInterface* mockArduino) { ::arduino = mockArduino; }
+
+void digitalWrite(const uint8_t& pin, const int& value)
+{
+    if (::arduino != nullptr)
+    {
+        ::arduino->_digitalWrite(pin, value);
+    }
+}
+
+int digitalRead(const uint8_t& pin)
+{
+    if (::arduino != nullptr)
+    {
+        return ::arduino->_digitalRead(pin);
+    }
+    return 0;
+}
+
+void analogWrite(const uint8_t& pin, const int& value)
+{
+    if (::arduino != nullptr)
+    {
+        ::arduino->_analogWrite(pin, value);
+    }
+}
+
+int analogRead(const uint8_t& pin)
+{
+    if (::arduino != nullptr)
+    {
+        return ::arduino->_analogRead(pin);
+    }
+    return 0;
+}
+
+void shiftOut(const uint8_t& dataPin, const uint8_t& clockPin, const int& direction, const int& data)
+{
+    if (::arduino != nullptr)
+    {
+        ::arduino->_shiftOut(dataPin, clockPin, direction, data);
+    }
+}
+
+void pinMode(const uint8_t& pin, const int& direction)
+{
+    if (::arduino != nullptr)
+    {
+        ::arduino->_pinMode(pin, direction);
+    }
+}
