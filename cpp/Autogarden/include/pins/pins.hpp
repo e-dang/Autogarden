57ac2f0179b9c6568678a5f9df195fd515eace63
Eric Dang <edang830@gmail.com>
2021-02-15 16:06:09 -0800

    Add convenience headers

---
+++ cpp/Autogarden/include/pins/pins.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <pins/logic_input.hpp>
+#include <pins/logic_input_pinset.hpp>
+#include <pins/logic_output_pinset.hpp>
+#include <pins/terminal.hpp>
+#include <pins/terminal_pinset.hpp>
---
+++ cpp/Autogarden/include/signals/signals.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <signals/analog_read.hpp>
+#include <signals/analog_write.hpp>
+#include <signals/digital_read.hpp>
+#include <signals/digital_write.hpp>
