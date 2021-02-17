fc4e4d53e2602f42b67478b9a80432b311db3326
Eric Dang <edang830@gmail.com>
2021-02-17 14:18:12 -0800

    Make Component inherit from interface

--- cpp/Autogarden/include/components/component.hpp
+++ cpp/Autogarden/include/components/component.hpp
@@ -3,13 +3,31 @@
 #include <pins/pins.hpp>
 #include <string>
 
-class Component {
+class Component;
+
+class IComponent {
+public:
+    virtual ~IComponent() = default;
+
+    virtual bool appendChild(Component* component) = 0;
+
+    virtual Component* getChild(const std::string& id) = 0;
+
+    virtual int getNumChildren() const = 0;
+
+    virtual const Component* getParent() const = 0;
+
+    virtual std::string getId() const = 0;
+
+    virtual bool hasParent() const = 0;
+};
+class Component : public IComponent {
 public:
     Component(const std::string& id) : __mId(id), _pParent(nullptr), __mChildren(0) {}
 
     virtual ~Component() = default;
 
-    bool appendChild(Component* component) {
+    bool appendChild(Component* component) override {
         if (component != nullptr && component->_setInputPins(_getOutputPins())) {
             __mChildren.push_back(component);
             component->_pParent = this;
@@ -19,7 +37,7 @@ public:
         return false;
     }
 
-    Component* getChild(const std::string& id) {
+    Component* getChild(const std::string& id) override {
         if (getId() == id)
             return this;
 
@@ -32,19 +50,19 @@ public:
         return nullptr;
     }
 
-    int getNumChildren() const {
+    int getNumChildren() const override {
         return static_cast<int>(__mChildren.size());
     }
 
-    const Component* getParent() const {
+    const Component* getParent() const override {
         return _pParent;
     };
 
-    std::string getId() const {
+    std::string getId() const override {
         return __mId;
     }
 
-    bool hasParent() const {
+    bool hasParent() const override {
         return getParent() != nullptr;
     }
 
