ee1ef057c59774a9cd63da2f75248f55bab38daa
Eric Dang <edang830@gmail.com>
2021-02-19 16:09:13 -0800

    Refactor valve impl into DiStateActuator

--- cpp/Autogarden/include/components/valve/interfaces/valve.hpp
+++ cpp/Autogarden/include/components/valve/interfaces/valve.hpp
@@ -1,10 +1,11 @@
 #pragma once
 
-#include <components/component.hpp>
+#include <components/di_state_actuator.hpp>
 
-class IValve : public Component {
+class IValve : public DiStateActuator {
 public:
-    IValve(const std::string& id) : Component(id) {}
+    IValve(const std::string& id, ILogicInputPin* inputPin, const int& onValue, const int& offValue) :
+        DiStateActuator(id, inputPin, onValue, offValue) {}
 
     virtual ~IValve() = default;
 
--- cpp/Autogarden/include/components/valve/valve.hpp
+++ cpp/Autogarden/include/components/valve/valve.hpp
@@ -1,44 +1,17 @@
 #pragma once
 
-#include <components/component.hpp>
 #include <components/valve/interfaces/valve.hpp>
-#include <signals/signals.hpp>
 
 class Valve : public IValve {
 public:
     Valve(const std::string& id, ILogicInputPin* inputPin, const int& onValue = HIGH, const int& offValue = LOW) :
-        IValve(id), __pPin(inputPin), __mOnValue(onValue), __mOffValue(offValue) {}
+        IValve(id, inputPin, onValue, offValue) {}
 
     bool open() override {
-        return _performAction(__mOnValue);
+        return _performAction(_mOnValue);
     }
 
     bool close() override {
-        return _performAction(__mOffValue);
+        return _performAction(_mOffValue);
     }
-
-protected:
-    bool _setInputPins(Component* parent) override {
-        auto parentOutputPins = _getComponentOutputPins(parent);
-        if (parentOutputPins == nullptr)
-            return false;
-
-        return parentOutputPins->connect(__pPin.get());
-    }
-
-    IOutputPinSet* _getOutputPins() override {
-        return nullptr;
-    }
-
-    bool _performAction(const int& value) {
-        if (__pPin == nullptr || !__pPin->processSignal(std::make_shared<DigitalWrite>(value)))
-            return false;
-
-        return _propagateSignal();
-    }
-
-private:
-    int __mOnValue;
-    int __mOffValue;
-    std::unique_ptr<ILogicInputPin> __pPin;
 };
