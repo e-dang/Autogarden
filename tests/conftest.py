ed7a30a002850675316ea510bc95791aa5d7ad0b
Eric Dang <edang830@gmail.com>
2021-02-22 21:03:35 -0800

    Add headless option to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,27 @@
 import pytest
 from pytest_factoryboy import register
 from rest_framework.test import APIClient
+from selenium import webdriver
 
 from . import factories
+
+
+def pytest_addoption(parser):
+    parser.addoption('--headless', action='store_true', default=False)
+
+
+@pytest.fixture(scope='class')
+def driver_init(request):
+    if request.config.getoption('--headless'):
+        opts = webdriver.FirefoxOptions()
+        opts.add_argument('--headless')
+        request.cls.driver = webdriver.Firefox(options=opts)
+    else:
+        request.cls.driver = webdriver.Firefox()
+
+    yield
+
+    request.cls.driver.quit()
 
 
 @pytest.fixture(scope='session')
