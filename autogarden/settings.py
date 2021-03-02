cfb7d1ed59f79b5b411b0889dc9833fad667bd7d
Eric Dang <edang830@gmail.com>
2021-03-02 13:51:02 -0800

    Add dotenv requirement and setup email backend

--- autogarden/settings.py
+++ autogarden/settings.py
@@ -12,10 +12,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -141,3 +142,10 @@ MEDIA_ROOT = BASE_DIR / 'static' / 'images'
 AUTH_USER_MODEL = 'users.User'
 LOGIN_URL = 'login'
 AUTHENTICATION_BACKENDS = ['users.authentication.EmailBackend']
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ['EMAIL']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASSWORD']
