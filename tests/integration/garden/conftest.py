15eef543ea38572620ee77dc75251e3798ba2936
Eric Dang <edang830@gmail.com>
2021-03-04 12:56:05 -0800

    Implement json factories

--- tests/integration/garden/conftest.py
+++ tests/integration/garden/conftest.py
@@ -1,37 +1,30 @@
+from random import randint
+
 import pytest
 from django.core.files.uploadedfile import SimpleUploadedFile
 from rest_framework.test import APIClient
 from tests.conftest import TEST_IMAGE_DIR
 
-from garden.utils import build_duration_string
+
+@pytest.fixture
+def watering_station_form_fields(watering_station_factory):
+    return watering_station_factory.form_fields()
 
 
 @pytest.fixture
-def valid_watering_station_data():
-    return {'moisture_threshold': 89,
-            'watering_duration': build_duration_string(5, 65),
-            'plant_type': 'lettuce',
-            'status': True
-            }
+def new_garden_form_fields(garden_factory):
+    data = garden_factory.form_fields()
+    data['num_watering_stations'] = randint(0, 10)
+    return data
 
 
 @pytest.fixture
-def valid_garden_data():
-    return {'name': 'My Garden',
-            'num_watering_stations': 3,
-            'update_interval': '10:00'}
-
-
-@pytest.fixture
-def valid_update_garden_data(use_tmp_static_dir):
+def valid_update_garden_data(garden_factory, use_tmp_static_dir):
     image_path = str(TEST_IMAGE_DIR / 'test_garden_image.png')
     with open(image_path, 'rb') as f:
-        file = SimpleUploadedFile('test_garden_image.png', f.read(), content_type='image/png')
-        return {
-            'name': 'new garden name',
-            'update_interval': '10:00',
-            'image': file
-        }
+        data = garden_factory.form_fields()
+        data['image'] = SimpleUploadedFile('test_garden_image.png', f.read(), content_type='image/png')
+        return data
 
 
 @pytest.fixture
