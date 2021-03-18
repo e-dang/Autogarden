9e516d70d1a0d137d955206130d1ec06906ff164
Eric Dang <edang830@gmail.com>
2021-03-18 16:02:42 -0700

    Update interface and use apikey instead of pk

--- cpp/Autogarden/include/client/api_client.hpp
+++ cpp/Autogarden/include/client/api_client.hpp
@@ -9,29 +9,16 @@
 
 class APIClient : public IAPIClient {
 public:
-    APIClient(const String& rootUrl, std::unique_ptr<IHttpClient>&& client,
+    APIClient(const String& apiKey, const String& rootUrl, std::unique_ptr<IHttpClient>&& client,
               const String& contentType = "application/json") :
-        __mPK(""), __mRootUrl(rootUrl), __mContentType(contentType), __pClient(std::move(client)) {}
+        __mAPIKey(apiKey), __mRootUrl(rootUrl), __mContentType(contentType), __pClient(std::move(client)) {}
 
-    void initializeServer(const DynamicJsonDocument& data) override {
+    DynamicJsonDocument getWateringStationConfigs() override {
         HttpRequest request;
-        request.url         = getInitializationUrl();
-        request.method      = "post";
-        request.contentType = __mContentType;
-        request.data        = data;
-
-        while (String(__mPK.toInt()) != __mPK) {
-            auto response = __pClient->post(request);
-            if (response.statusCode > 0)
-                __setPk(response.getJsonData());
-        }
-    }
-
-    DynamicJsonDocument fetchConfigs() override {
-        HttpRequest request;
-        request.url         = getWateringStationsUrl();
-        request.method      = "get";
-        request.contentType = __mContentType;
+        request.url           = _getWateringStationsUrl();
+        request.method        = "get";
+        request.contentType   = __mContentType;
+        request.authorization = __mAPIKey;
 
         HttpResponse response;
         while (true) {
@@ -43,25 +30,74 @@ public:
         return response.getJsonData();
     }
 
-    String getWateringStationsUrl() const override {
-        auto url = getInitializationUrl() + "<?>/watering-stations/";
-        url.replace("<?>", __mPK);
+    DynamicJsonDocument getGardenConfigs() override {
+        HttpRequest request;
+        request.url           = _getGardenUrl();
+        request.method        = "get";
+        request.contentType   = __mContentType;
+        request.authorization = __mAPIKey;
+
+        HttpResponse response;
+        while (true) {
+            response = __pClient->get(request);
+            if (response.statusCode > 0)
+                break;
+        }
+
+        return response.getJsonData();
+    }
+
+    void sendGardenData(const DynamicJsonDocument& data) const override {
+        HttpRequest request;
+        request.url           = _getGardenUrl();
+        request.method        = "patch";
+        request.contentType   = __mContentType;
+        request.authorization = __mAPIKey;
+        request.data          = data;
+
+        HttpResponse response;
+        while (true) {
+            response = __pClient->patch(request);
+            if (response.statusCode > 0)
+                break;
+        }
+    }
+
+    void sendWateringStationData(const DynamicJsonDocument& data) const override {
+        HttpRequest request;
+        request.url           = _getGardenUrl();
+        request.method        = "post";
+        request.contentType   = __mContentType;
+        request.authorization = __mAPIKey;
+        request.data          = data;
+
+        HttpResponse response;
+        while (true) {
+            response = __pClient->post(request);
+            if (response.statusCode > 0)
+                break;
+        }
+    }
+
+    int getConnectionStrength() const override {
+        return __pClient->getConnectionStrength()
+    }
+
+protected:
+    String _getWateringStationsUrl() const {
+        return _getGardenUrl() + "watering-stations/";
+    }
+
+    String _getGardenUrl() const {
+        auto url = __mRootUrl + "api/gardens/<?>/";
+        url.replace("<?>", __mAPIKey);
         return url;
     }
 
-    String getInitializationUrl() const override {
-        return __mRootUrl + "api/garden/";
-    }
-
 private:
-    void __setPk(const DynamicJsonDocument& data) {
-        __mPK = data["pk"].as<String>();
-    }
-
-private:
-    String __mPK;
     String __mRootUrl;
     String __mContentType;
+    String __mAPIKey;
     std::unique_ptr<IHttpClient> __pClient;
 };
 
@@ -70,10 +106,10 @@ public:
     APIClientFactory(std::unique_ptr<IHttpClientFactory>&& httpClientFactory) :
         __pHttpClientFactory(std::move(httpClientFactory)) {}
 
-    std::unique_ptr<IAPIClient> create(const String& ssid, const String& password, const String& rootUrl,
-                                       const int& waitTime = 1000) override {
+    std::unique_ptr<IAPIClient> create(const String&& apiKey, const String& ssid, const String& password,
+                                       const String& rootUrl, const int& waitTime = 1000) override {
         auto httpClient = __pHttpClientFactory->create(ssid, password, waitTime);
-        return std::make_unique<APIClient>(rootUrl, std::move(httpClient));
+        return std::make_unique<APIClient>(apiKey, rootUrl, std::move(httpClient));
     }
 
 private:
